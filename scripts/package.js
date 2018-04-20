6ce105b02d019976a9c56fbedb73e1e98a6a0f50
xzper <mail@xzper.com>
2018-04-20 17:14:44 +0800

    packager

---
+++ scripts/package.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const packager = require('electron-packager');
+
+const includeFiles = [
+	'out',
+	'resources',
+	'package.json'
+];
+
+function ignore(file) {
+	if (!file) {
+		return false;
+	}
+	if (file.startsWith('/')) {
+		file = file.substr(1);
+	}
+	const willCopy = includeFiles.filter(include => {
+		return file.startsWith(include);
+	});
+	if (willCopy.length > 0) {
+		return false;
+	}
+	return true;
+}
+
+packager({
+	name: 'Shopify',
+	dir: path.join(__dirname, '..'),
+	out: 'packager',
+	asar: true,
+	overwrite: true,
+	prune: false,
+	ignore: ignore,
+	extraResource: [
+	]
+}, (error, appPaths) => {
+	if (error) {
+		return error;
+	} else {
+		console.log(appPaths);
+	}
+});
